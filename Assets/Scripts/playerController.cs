a15d84f125aaddad986d831a21f32dfd75becb28
NathanWGDev <NWathingira@student.fullsail.edu>
2023-01-09 14:06:32 -0500

    added sprint
    sprint works for player

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -9,43 +9,86 @@ public class playerController : MonoBehaviour
 
     [Header("----- Player Stats -----")]
     [Range(1, 10)] [SerializeField] int HP;
-    [Range(1, 10)] [SerializeField] int playerSpeed;
+    [Range(1, 10)] [SerializeField] int walkSpeed;
+    [Range(10,40)][SerializeField] int sprintSpeed;
     [Range(5, 20)] [SerializeField] int jumpAmount;
     [Range(5, 50)] [SerializeField] int gravity;
     [Range(1, 5)] [SerializeField] int jumpsAllowed;
 
+    [Header("----- Test View -----")]
+    [SerializeField] private float playerSpeed;
+
     int jumpedTimes;
+
+    bool isSprinting;
+    bool isJumping;
+
     Vector3 move;
     Vector3 playerVelocity;
 
     void Update()
     {
-        Movement();
+        float delta = Time.deltaTime;
+
+        HandleInputs();
+        Movement(delta);
     }
 
-    void Movement()
+    void Movement(float delta)
     {
         if (controller.isGrounded && playerVelocity.y < 0)
         {
             playerVelocity.y = 0;
             jumpedTimes = 0;
         }
+       
+        if (isSprinting)
+        {
+            playerSpeed = delta * sprintSpeed;
+        }
+        else
+        {
+            playerSpeed = delta * walkSpeed;
+        }
+        
+        controller.Move(move * playerSpeed);
 
-        move = (transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (Input.GetButtonDown("Jump") && jumpedTimes < jumpsAllowed)
+        if (isJumping)
         {
             playerVelocity.y = jumpAmount;
             jumpedTimes++;
         }
-
-        playerVelocity.y -= gravity * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        
+        
+        playerVelocity.y -= gravity * delta;
+        controller.Move(playerVelocity * delta);
     }
 
     public void TakeDamage(int dmg)
     {
         HP -= dmg;
+    }
+
+    private void HandleInputs()
+    {
+        move = (transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"));
+
+        if (Input.GetButtonDown("Jump") && jumpedTimes < jumpsAllowed)
+        {
+            isJumping = true;
+        }
+        else
+        {
+            isJumping = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
     }
 }
