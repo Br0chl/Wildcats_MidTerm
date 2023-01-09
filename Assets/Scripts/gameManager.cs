7f2a99fdd3efe6fa04590a76044e936700d24a8b
NathanWGDev <NWathingira@student.fullsail.edu>
2023-01-08 19:11:14 -0500

    Put some scripts
    Added the isDamageable, EnemyAI, and gameManager cs

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    public GameObject player;
+    public playerController playerScript;
+    public int enemiesRemaining;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void updateEnemyRemaining(int amount)
+    {
+        enemiesRemaining += amount;
+
+        // check to see if game is over based on enemy count <=0
+    }
+}
+
