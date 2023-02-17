65a320a2189590c1f85209532175217987204c54
ArnnPowell <aapowell@student.fullsail.edu>
2023-02-16 22:53:38 -0600

    Sound Options Fixed
    PlayerPrefs now load when parameters do not exist, and carry over all levels

--- Assets/Scripts/SoundSettings.cs
+++ Assets/Scripts/SoundSettings.cs
@@ -12,14 +12,24 @@ public class SoundSettings : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         master = PlayerPrefs.GetFloat("MasterVolume");
         music = PlayerPrefs.GetFloat("MusicVolume");
         sfx = PlayerPrefs.GetFloat("SFXVolume");
 
-        main.SetFloat("MasterVolume", master);
-        main.SetFloat("MusicVolume", music);
-        main.SetFloat("SFXVolume", sfx);
+        if (!PlayerPrefs.HasKey("MasterVolume"))
+        {
+            PlayerPrefs.SetFloat("MasterVolume", .5f);
+            PlayerPrefs.SetFloat("MusicVolume", .5f);
+            PlayerPrefs.SetFloat("SFXVolume", .5f);
+        }
+
+        main.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
+        main.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
+        main.SetFloat("SFXVolume", PlayerPrefs.GetFloat("SFXVolume"));
+        // main.SetFloat("MasterVolume", master);
+        // main.SetFloat("MusicVolume", music);
+        // main.SetFloat("SFXVolume", sfx);
     }
 }
